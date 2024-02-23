a7a7e0228014e4bff920bd458e6c12767cdac301
Sloth <70744076+Slothric@users.noreply.github.com>
2024-02-22 20:02:21 -0500

    style: small change to hover animation on table rows

--- kiln-trace/src/components/pieceEntry.tsx
+++ kiln-trace/src/components/pieceEntry.tsx
@@ -3,15 +3,15 @@ import { TableCell, TableRow } from "@/components/ui/table";
 function PieceEntry() {
   return (
     <>
-      <TableRow>
-        <TableCell className="text-black font-medium">Today</TableCell>
-        <TableCell className="text-black font-medium">A title</TableCell>
-        <TableCell className="text-black font-medium">Bisqued</TableCell>
-        <TableCell className="text-black font-medium">Thrown</TableCell>
-        <TableCell className="text-black font-medium">Vase</TableCell>
-        <TableCell className="text-black font-medium">12 lbs</TableCell>
-        <TableCell className="text-black font-medium">30'x20'x15'</TableCell>
-        <TableCell className="text-black font-medium">
+      <TableRow className="trow">
+        <TableCell className=" text-black font-medium">Today</TableCell>
+        <TableCell className=" text-black font-medium">A title</TableCell>
+        <TableCell className=" text-black font-medium">Bisqued</TableCell>
+        <TableCell className=" text-black font-medium">Thrown</TableCell>
+        <TableCell className=" text-black font-medium">Vase</TableCell>
+        <TableCell className=" text-black font-medium">12 lbs</TableCell>
+        <TableCell className=" text-black font-medium">30'x20'x15'</TableCell>
+        <TableCell className=" text-black font-medium">
           Pottery Peacock
         </TableCell>
         <TableCell className="text-black font-medium">Generica</TableCell>
