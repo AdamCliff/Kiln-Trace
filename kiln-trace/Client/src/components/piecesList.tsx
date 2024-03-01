ed506e7ba4df9bb9c90caa5acbabe67ce108f0c6
Sloth <70744076+Slothric@users.noreply.github.com>
2024-02-29 21:20:56 -0500

    Style: Removed some text and replaced it with icons

--- kiln-trace/Client/src/components/piecesList.tsx
+++ kiln-trace/Client/src/components/piecesList.tsx
@@ -38,9 +38,22 @@ function PiecesList() {
             <Link
               to={"/"}
               id="new-piece-btn"
-              className="py-2 px-4 bg-mutedGreen rounded-xl"
+              className="flex items-center justify-center h-min w-min p-2 bg-mutedGreen rounded-xl"
             >
-              +
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
             </Link>
           </TableCaption>
           <TableHeader>
